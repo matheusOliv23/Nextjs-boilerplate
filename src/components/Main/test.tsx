847ad01cd03c15a70dc7bccecc0d24f1942fefb3
matheusOliv23 <matheushenri1996@gmail.com>
2022-09-03 09:59:25 -0300

    feat: generator default files

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,18 +1,16 @@
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import Main from "./index"
-
 
 describe("Main", () => {
   it("should render heading", () => {
-    render(
+    const { container } = render(
       <Main
         title="Boilerplate"
         description="Typescript, Reactjs, Nextjs, Styled Components"
       />
     )
-    expect(
-      screen.getByRole("heading", { name: /Boilerplate/i })
-    ).toBeInTheDocument()
+    expect(container).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
   })
 
   it("should render colors correctly", () => {
@@ -23,15 +21,5 @@ describe("Main", () => {
       />
     )
     expect(container.firstChild).toHaveStyle({ "background-color": "#06092b" })
-  })
-
-  it("should snapshot", () => {
-    const { container } = render(
-      <Main
-        title="Boilerplate"
-        description="Typescript, Reactjs, Nextjs, Styled Components"
-      />
-    )
-    expect(container).toMatchSnapshot()
   })
 })

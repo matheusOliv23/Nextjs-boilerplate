590b40f5dd454e1e1aa7bc694cc6dde320c68901
matheusOliv23 <matheushenri1996@gmail.com>
2022-09-03 17:03:13 -0300

    fix: jest configs

--- babel.config.js
+++ babel.config.js
@@ -7,4 +7,22 @@ module.exports = {
         }],
         '@babel/preset-typescript',
     ],
+    plugins: [
+        ["babel-plugin-styled-components", {
+            "ssr": true,
+            "displayName": true
+        }]
+    ],
+    "env": {
+        "test": {
+            "plugins": [
+                [
+                    "babel-plugin-styled-components", {
+                        'ssr': false,
+                        'displayName': false
+                    }
+                ]
+            ]
+        }
+    }
 };
